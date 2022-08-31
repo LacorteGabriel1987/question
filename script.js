f7c14bdf5a98dbb2b41ea9e7250693372b9b2add
Gabriel Lacorte <gabrielslacorte@hotmail.com>
2022-08-31 16:28:33 -0300

    Finalizando js

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-var area = window.prompt ("Qual área deseja seguir, Front-End, ou Back-End? ");
-    
-var msg = (`Você gosta do ${area} que legal!`)
-
-alert(msg);
-
-var gosta = window.confirm(`Você gosta de estudar ${area}?`)
-
-function res (){    
-
-    if(area == Front-End){
-       alert ("Você pode aprender React ou Vue")
-    } else {
-        alert("Ou, Como Back-End você pode aprender c# ou Java")
-    } 
+ const area = prompt("Você quer seguir para área de 'Front-End' ou de 'Back-End'? Digite o nome da área:");
+let linguagem = "";
+if (area === "Front-End"){
+    linguagem = prompt("Você quer aprender React ou Vue?");
 }
- res()
+else if (area === "Back-End"){
+    linguagem = prompt("Você quer aprender C# ou Java?");
+}
+else {
+    alert("Você não inseriu uma área válida!");
+}
+
+const especialidadeOuFullstack = prompt("Digite 1 para seguir se especializando na área escolhida ou 2 para seguir se desenvolvendo para se tornar Fullstack");
+if (especialidadeOuFullstack == 1){
+    alert(`Continue se especializando em ${linguagem} para dominar a área de ${area}!`);
+}
+else if (especialidadeOuFullstack == 2){
+    alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`);
+}
+else {
+    alert("Você não inseriu um valor válido!");
+}
+
+let msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
+while (msg === "ok"){
+    let novaTecnologia = prompt("Qual?");
+    alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`)
+    msg = prompt("Tem mais alguma tecnologia que você gostaria de aprender? Digite 'ok' em caso positivo.");
+}
